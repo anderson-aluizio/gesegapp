1cbc6a933f94bdf49279031f396b79666057445c
anderson.nogueira <anderson.nogueira@dinamo.srv.br>
2025-10-20 08:47:36 -0300

    feat(sentry): integrate Sentry for error tracking and performance monitoring

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
-import { Slot, Stack } from "expo-router"
+import { Stack } from "expo-router"
 import { SQLiteProvider } from "expo-sqlite"
 
-import { initializeDatabase, safeInitializeDatabase } from "@/database/databaseSchema"
+import { safeInitializeDatabase } from "@/database/databaseSchema"
 import { PaperProvider } from "react-native-paper"
 import { StatusBar } from "expo-status-bar"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
@@ -9,6 +9,27 @@ import { AuthProvider } from "@/contexts/AuthContext"
 import { UpdateProvider } from "@/contexts/UpdateContext"
 import { AutocompleteDropdownContextProvider } from 'react-native-autocomplete-dropdown'
 import TurnoGuard from "@/components/TurnoGuard"
+
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://3126b8211b8ed703c188bc1042486e98@o1215639.ingest.us.sentry.io/4510221608091648',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 export default function Layout() {
   return (
